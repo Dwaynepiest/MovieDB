7eb607e20abd8a609b2cd17f026292a11f0255bc
Dwaynepiest <dwaynepiest@outlook.com>
2024-06-11 14:37:29 +0200

    Routes Vite+Vue geintalleerd en werkend

--- MovieDB/src/main.js
+++ MovieDB/src/main.js
@@ -2,4 +2,23 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Home from './components/home.vue'
+import About from './components/about.vue'
+import Contact from './components/contact.vue'
+
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+    { path: '/contact', component: Contact }
+  ]
+})
+
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
