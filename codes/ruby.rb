ca808eacd36ac452e4f49de00d93a6a8026305c5
Dwaynepiest <dwaynepiest@outlook.com>
2024-06-17 10:36:06 +0200

    frwdiop

--- codes/ruby.rb
+++ codes/ruby.rb
@@ -9,3 +9,234 @@ def hallo_wereld(keer)
   # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
   hallo_wereld(3)
   
+  # HalloWereldGeavanceerd.rb
+
+def hallo_wereld(keer)
+    keer.times do
+      puts "Hallo, wereld!"
+    end
+  end
+  
+  # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
+  hallo_wereld(3)
+  # HalloWereldGeavanceerd.rb
+
+def hallo_wereld(keer)
+    keer.times do
+      puts "Hallo, wereld!"
+    end
+  end
+  
+  # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
+  hallo_wereld(3)
+  # HalloWereldGeavanceerd.rb
+
+def hallo_wereld(keer)
+    keer.times do
+      puts "Hallo, wereld!"
+    end
+  end
+  
+  # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
+  hallo_wereld(3)
+  # HalloWereldGeavanceerd.rb
+
+def hallo_wereld(keer)
+    keer.times do
+      puts "Hallo, wereld!"
+    end
+  end
+  
+  # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
+  hallo_wereld(3)
+  # HalloWereldGeavanceerd.rb
+
+def hallo_wereld(keer)
+    keer.times do
+      puts "Hallo, wereld!"
+    end
+  end
+  
+  # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
+  hallo_wereld(3)
+  # HalloWereldGeavanceerd.rb
+
+def hallo_wereld(keer)
+    keer.times do
+      puts "Hallo, wereld!"
+    end
+  end
+  
+  # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
+  hallo_wereld(3)
+  # HalloWereldGeavanceerd.rb
+
+def hallo_wereld(keer)
+    keer.times do
+      puts "Hallo, wereld!"
+    end
+  end
+  
+  # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
+  hallo_wereld(3)
+  # HalloWereldGeavanceerd.rb
+
+def hallo_wereld(keer)
+    keer.times do
+      puts "Hallo, wereld!"
+    end
+  end
+  
+  # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
+  hallo_wereld(3)
+  # HalloWereldGeavanceerd.rb
+
+def hallo_wereld(keer)
+    keer.times do
+      puts "Hallo, wereld!"
+    end
+  end
+  
+  # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
+  hallo_wereld(3)
+  # HalloWereldGeavanceerd.rb
+
+def hallo_wereld(keer)
+    keer.times do
+      puts "Hallo, wereld!"
+    end
+  end
+  
+  # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
+  hallo_wereld(3)
+  # HalloWereldGeavanceerd.rb
+
+def hallo_wereld(keer)
+    keer.times do
+      puts "Hallo, wereld!"
+    end
+  end
+  
+  # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
+  hallo_wereld(3)
+  # HalloWereldGeavanceerd.rb
+
+def hallo_wereld(keer)
+    keer.times do
+      puts "Hallo, wereld!"
+    end
+  end
+  
+  # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
+  hallo_wereld(3)
+  # HalloWereldGeavanceerd.rb
+
+def hallo_wereld(keer)
+    keer.times do
+      puts "Hallo, wereld!"
+    end
+  end
+  
+  # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
+  hallo_wereld(3)
+  # HalloWereldGeavanceerd.rb
+
+def hallo_wereld(keer)
+    keer.times do
+      puts "Hallo, wereld!"
+    end
+  end
+  
+  # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
+  hallo_wereld(3)
+  # HalloWereldGeavanceerd.rb
+
+def hallo_wereld(keer)
+    keer.times do
+      puts "Hallo, wereld!"
+    end
+  end
+  
+  # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
+  hallo_wereld(3)
+  # HalloWereldGeavanceerd.rb
+
+def hallo_wereld(keer)
+    keer.times do
+      puts "Hallo, wereld!"
+    end
+  end
+  
+  # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
+  hallo_wereld(3)
+  # HalloWereldGeavanceerd.rb
+
+def hallo_wereld(keer)
+    keer.times do
+      puts "Hallo, wereld!"
+    end
+  end
+  
+  # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
+  hallo_wereld(3)
+  # HalloWereldGeavanceerd.rb
+
+def hallo_wereld(keer)
+    keer.times do
+      puts "Hallo, wereld!"
+    end
+  end
+  
+  # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
+  hallo_wereld(3)
+  # HalloWereldGeavanceerd.rb
+
+def hallo_wereld(keer)
+    keer.times do
+      puts "Hallo, wereld!"
+    end
+  end
+  
+  # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
+  hallo_wereld(3)
+  # HalloWereldGeavanceerd.rb
+
+def hallo_wereld(keer)
+    keer.times do
+      puts "Hallo, wereld!"
+    end
+  end
+  
+  # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
+  hallo_wereld(3)
+  # HalloWereldGeavanceerd.rb
+
+def hallo_wereld(keer)
+    keer.times do
+      puts "Hallo, wereld!"
+    end
+  end
+  
+  # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
+  hallo_wereld(3)
+  # HalloWereldGeavanceerd.rb
+
+def hallo_wereld(keer)
+    keer.times do
+      puts "Hallo, wereld!"
+    end
+  end
+  
+  # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
+  hallo_wereld(3)
+  # HalloWereldGeavanceerd.rb
+
+def hallo_wereld(keer)
+    keer.times do
+      puts "Hallo, wereld!"
+    end
+  end
+  
+  # Roept de functie aan om "Hallo, wereld!" 3 keer af te drukken
+  hallo_wereld(3)
+  
