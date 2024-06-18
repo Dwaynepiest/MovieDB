90e63090a8696549646d42faead412723517c01d
W0ppert <jori_gaming@outlook.com>
2024-06-18 11:00:32 +0200

    route fix

--- MovieDB/src/main.js
+++ MovieDB/src/main.js
@@ -5,20 +5,12 @@ import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from './components/home.vue'
-import Favorite from './components/favorite.vue'
-import Search from './components/search.vue'
-import Settings from './components/settings.vue'
-import Profile from './components/profile.vue'
 
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: Home },
-    { path: '/favorite', component: Favorite },
-    { path: '/search', component: Search },
-    { path: '/settings', component: Settings },
-    { path: '/profile', component: Profile }
   ]
 })
 
