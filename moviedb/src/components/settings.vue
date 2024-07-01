b8bb0944fbe09e19126b7def44738a0f067a521f
Dwaynepiest <dwaynepiest@outlook.com>
2024-07-01 10:34:51 +0200

    SETINGS

--- moviedb/src/components/settings.vue
+++ moviedb/src/components/settings.vue
@@ -1,6 +1,32 @@
 <script setup>
 
 </script>
-<template>
-settings
+    <template>
+  <div class="form-container">
+    <form>
+      <div class="all_movies">
+        <label for="all_movies">All Movies</label>
+        <input type="text" id="all_movies" />
+      </div>
+      <div class="movie_type">
+        <label for="movie_type">By Movie Type</label>
+        <input type="text" id="movie_type" />
+      </div>
+      <div class="genres">
+        <label for="genres">By Genre</label>
+        <input type="text" id="genres" />
+      </div>
+      <div class="favorites">
+        <label for="favorites">Favorites</label>
+        <input type="text" id="favorites" />
+      </div>
+      <div class="movie">
+        <label for="movie">Movie</label>
+        <input type="text" id="movie" />
+      </div>
+      <div class="sumbit">
+        <input type="button" value="Save" id="save">
+      </div>
+    </form>
+  </div>
 </template>
